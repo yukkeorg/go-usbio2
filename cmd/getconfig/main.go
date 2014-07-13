0d5e3dfd1068614ea2a0f3b2ef00a0aa4e14244b
Yusuke Ohshima <git@yukke.org>
2014-07-13 23:21:15 +0900

    Add sample command that is getting and showing USB-IO2 configuration

---
+++ cmd/getconfig/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "log"
+    "github.com/yukkeorg/go-usbio2"
+)
+
+func main() {
+    uio, err := usbio2.NewUsbIO2()
+    if err != nil {
+        return
+    }
+
+    defer uio.Cleanup()
+
+    config, err := uio.GetConfig()
+    if err != nil {
+        return
+    }
+
+    log.Printf("Detected: %s", uio.GetDeviceName())
+    log.Printf("Pullup Enabled: %v", config.EnablePullUp)
+    log.Printf("Port1 Pin Setup (value in 'ture' is a INPUT)")
+    for i, b := range config.PinPort1 {
+        log.Printf("  Pin%d: %v", i, b)
+    }
+    log.Printf("Port2 Pin Setup (value in 'ture' is a INPUT)")
+    for i, b := range config.PinPort2 {
+        log.Printf("  Pin%d: %v", i, b)
+    }
+}
