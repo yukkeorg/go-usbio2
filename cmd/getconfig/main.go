52b77b5336a84f2d0fdc22d6d75a7cc8228e3c61
Yusuke Ohshima <ohshima.yusuke@yukke.org>
2020-11-01 12:24:17 +0900

    cmd/getconfig: Tabbed

--- cmd/getconfig/main.go
+++ cmd/getconfig/main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-    "log"
-    "github.com/yukkeorg/go-usbio2"
+	"log"
+	"github.com/yukkeorg/go-usbio2"
 )
 
 func main() {
-    usbio, err := usbio2.NewUsbIO2()
-    if err != nil {
+	usbio, err := usbio2.NewUsbIO2()
+	if err != nil {
 		log.Printf("USB-IO2.0 not detected.")
-        return
-    }
+		return
+	}
+	defer usbio.Cleanup()
 
-    defer usbio.Cleanup()
-
-    config, err := usbio.GetConfig()
-    if err != nil {
+	config, err := usbio.GetConfig()
+	if err != nil {
 		log.Printf("Can't read device config.")
-        return
-    }
+		return
+	}
 
-    log.Printf("Detected device: %s", usbio.GetDeviceName())
-    log.Printf("Pullup Enabled: %v", config.EnablePullUp)
-    log.Printf("Port1 Pin Setup (value in 'ture' is a INPUT)")
-    for i, b := range config.Port1 {
-        log.Printf("  Pin%d: %v", i, b)
-    }
-    log.Printf("Port2 Pin Setup (value in 'ture' is a INPUT)")
-    for i, b := range config.Port2 {
-        log.Printf("  Pin%d: %v", i, b)
-    }
+	log.Printf("Detected device: %s", usbio.GetDeviceName())
+	log.Printf("Pullup Enabled: %s", bool2str(config.EnablePullUp, "ENABLE", "DISABLE"))
+	log.Printf("Port1 Pin Setup")
+	for i, b := range config.Port1 {
+		log.Printf("  Pin%d: %s", i, bool2str(b, "INPUT", "OUTPUT"))
+	}
+	log.Printf("Port2 Pin Setup")
+	for i, b := range config.Port2 {
+		log.Printf("  Pin%d: %s", i, bool2str(b, "INPUT", "OUTPUT"))
+	}
+}
+
+func bool2str(b bool, true_str string, false_str string) string {
+	if(b) {
+		return true_str
+	} else {
+		return false_str
+	}
 }
